8ed6ea73504e9a50484bf13501d6dc68bf4e155d
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-09-23 08:09:13 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = 24 % 5;
+let max = Math.max(1, 2, 3, 4, 20, 15);
